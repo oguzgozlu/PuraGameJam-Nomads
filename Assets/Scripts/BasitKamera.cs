9de8881dc62f2525d6dccbbd4768a79b843d8ee7
Barış Günenç <power.of.baris@gmail.com>
2020-12-26 02:03:41 +0300

    First commit

---
+++ Assets/Scripts/BasitKamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasitKamera : MonoBehaviour
+{
+    [SerializeField]
+    float sens = 120f;
+
+    public GameObject oyuncuAna;
+
+    float xEkseni = 0f;
+
+    public bool isCursorLocked = true;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (isCursorLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+
+        xEkseni -= mouseY;
+        xEkseni = Mathf.Clamp(xEkseni, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xEkseni, 0f, 0f);
+
+        oyuncuAna.transform.Rotate(Vector3.up * mouseX);
+        
+
+    }
+
+
+}
---
+++ Assets/Scripts/OyuncuHareket.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyuncuHareket : MonoBehaviour
+{
+
+    public GameObject groundChecker;
+    [HideInInspector]
+    public CharacterController control;
+
+    public float gravity = -9.81f;
+    public float hiz = 10f;
+    public float groundCheckSize = 0.4f;
+    public float jHeight = 4f;
+
+
+    public LayerMask groundMask;
+
+    //------------Privates-------------
+    Vector3 gravityVelocity;
+    bool isGrounded;
+
+    //-------------------------
+    // Start is called before the first frame update
+    void Start()
+    {
+      control = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundChecker.transform.position, groundCheckSize, groundMask);
+
+        if (isGrounded && gravityVelocity.y <0)
+        {
+            gravityVelocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 mov = transform.right * x + transform.forward * z;
+
+
+        control.Move(mov * hiz * Time.deltaTime);
+
+        gravityVelocity.y += gravity * Time.deltaTime;
+
+        control.Move(gravityVelocity * Time.deltaTime);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            gravityVelocity.y = Mathf.Sqrt(jHeight * -2f * gravity);
+        }
+
+
+    }
+}
